3772108f4073b90a84b6d3a8d324d22cff6ddf76
AskNilesh <rathodnilsrk@gmail.com>
2023-05-19 22:08:32 +0530

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Nilesh Rathod on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
